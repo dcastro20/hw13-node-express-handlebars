d4a684bda22d626768b3f1abeed810b774f774ef
Doris Castro <dorisgeo12@gmail.com>
2020-05-30 13:23:23 -0400

    added the seed.sql and run was successful

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Killer Baskin Burger');
+INSERT INTO burgers (burger_name) VALUES ('Exotic Mullet Burger');
+INSERT INTO burgers (burger_name) VALUES ('I Slaw a Tiger Burger');
+INSERT INTO burgers (burger_name) VALUES ('Here Kitty-Kitty Burger');
